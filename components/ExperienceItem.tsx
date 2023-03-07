abe2d1d960a8c2db3916bdc35dd9e5467a982a8b
Paul Buscano <pbuscano@deloitte.com>
2023-03-07 22:26:40 +0800

    [FIXED] job description

--- components/ExperienceItem.tsx
+++ components/ExperienceItem.tsx
@@ -1,14 +1,16 @@
-import { faArrowTrendUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
-import Link from "next/link";
-import urlFor from "../lib/urlFor";
+import { faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { PortableText } from '@portabletext/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import urlFor from '../lib/urlFor';
+import { RichTextComponents } from './RichTextComponents';
 
 type Props = {
   image: object;
   duration: string;
   company: string;
-  description: string;
+  description: Block[];
   position: string;
   link: string;
   key: string;
@@ -43,7 +45,9 @@ function ExperienceItem({
       <h4 className="">{company}</h4>
       <h6 className="text-xs mb-2 text-gray-600">{position}</h6>
       <p className="text-gray-400 text-sm mb-3">{duration}</p>
-      <p className="text-sm mb-5">{description}</p>
+      <div className="text-sm mb-5">
+        <PortableText value={description} components={RichTextComponents} />
+      </div>
       <Link
         href={`${link}`}
         target="_blank"
--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -1,9 +1,11 @@
-import { faArrowTrendUp } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Image from "next/image";
-import Link from "next/link";
-import urlFor from "../lib/urlFor";
-import ExperienceItem from "./ExperienceItem";
+import { faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { PortableText } from '@portabletext/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import urlFor from '../lib/urlFor';
+import ExperienceItem from './ExperienceItem';
+import { RichTextComponents } from './RichTextComponents';
 
 type Props = {
   experiences: Company[];
@@ -36,16 +38,12 @@ function Experiences({ experiences }: Props) {
               <h3 className="text-4xl mb-3">{experiences[0].company}</h3>
               <h5 className="mb-3 text-sm">{experiences[0].jobTitle}</h5>
               <p className="text-gray-400 mb-3">{experiences[0].duration}</p>
-              <p className="mb-5">
-                I recently developed a mobile application using iOS Swift and
-                SwiftUI, implemented the UI programmatically using UIKit, and
-                wrote unit tests using iOS Swift Test. To debug network calls, I
-                used Charles API Proxy and kept track of progress using Jira
-                Tickets. For the backend, I utilized Firebase functions using
-                ExpressJS and integrated with Azure DevOps repo. Additionally, I
-                also developed a cross-platform app using React-Native and a web
-                application using React and NextJS.
-              </p>
+              <div className="mb-5">
+                <PortableText
+                  value={experiences[0].jobDescription}
+                  components={RichTextComponents}
+                />
+              </div>
             </div>
             <a
               href={experiences[0].companyUrl}
@@ -68,7 +66,7 @@ function Experiences({ experiences }: Props) {
                 duration={filteredExperience.duration}
                 company={filteredExperience.company}
                 position={filteredExperience.jobTitle}
-                description="As a skilled React Native developer, I have implemented my own API collection using POSTMAN, utilized 3rd party npm packages, integrated react-navigation and react-native camera/gallery, applied Tailwind CSS, collaborated with backend developers, implemented Redux toolkit and authentication features like SMS OTP and Google Authenticator, and utilized Github and Microsoft app center for CI/CD, while reporting directly to the CTO and participating in weekly sprints."
+                description={filteredExperience.jobDescription}
               />
             ))}
           </div>
