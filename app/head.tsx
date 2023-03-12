5b17c19f64d60e64b8e5942fc20fa030dd55e0e9
Paul Buscano <pbuscano@deloitte.com>
2023-03-12 10:04:00 +0800

    [ADDED] google adsense head script

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,10 @@ export default function Head() {
       <title>Paul Lorenz Buscano</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6167557800097970"
+      ></script>
     </>
   );
 }
